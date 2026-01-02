ce8077988853ed8b540e8b0c4f9f41c92e6ed3a8
Dominik Jetzen <djetzen@users.noreply.github.com>
2026-01-02 15:30:56 +0100

    Day1 second part (#1)
    * implement secondpart of first day
    
    * refactor

--- src/main/java/de/advent/day1/Dial.java
+++ src/main/java/de/advent/day1/Dial.java
@@ -1,18 +1,37 @@
 package de.advent.day1;
 
+
 class Dial {
 
     private int currentValue = 50;
 
-    void rotate(String input) {
+    void rotateWithoutCountingZeroes(String input) {
         var rotationDirection = input.substring(0, 1);
+        validate(rotationDirection);
+        var valueToRotate = Integer.parseInt(input.substring(1));
         if (rotationDirection.equals("L")) {
-            rotateLeft(Integer.parseInt(input.substring(1)));
+            rotateLeft(valueToRotate);
         } else if (rotationDirection.equals("R")) {
-            rotateRight(Integer.parseInt(input.substring(1)));
-        } else {
-            throw new UnsupportedOperationException("must be prefixed with L or R");
+            rotateRight(valueToRotate);
         }
+    }
+
+    int rotateAndReturnNumberOfTimesPointedToZero(String input) {
+        var numberOfTimesZeroPassed = 0;
+        var rotationDirection = input.substring(0, 1);
+        validate(rotationDirection);
+        var valueToRotate = Integer.parseInt(input.substring(1));
+        for (int i = 0; i < valueToRotate; i++) {
+            if (rotationDirection.equals("L")) {
+                rotateSingleLeft();
+            } else if (rotationDirection.equals("R")) {
+                rotateSingleRight();
+            }
+            if (getCurrentValue() == 0) {
+                numberOfTimesZeroPassed++;
+            }
+        }
+        return numberOfTimesZeroPassed;
     }
 
     private void rotateLeft(int value) {
@@ -23,7 +42,21 @@ class Dial {
         currentValue += value;
     }
 
+    private void rotateSingleRight() {
+        rotateRight(1);
+    }
+
+    private void rotateSingleLeft() {
+        rotateLeft(1);
+    }
+
     int getCurrentValue() {
         return currentValue % 100;
+    }
+
+    private void validate(String rotationDirection) {
+        if (!rotationDirection.equals("L") && !rotationDirection.equals("R")) {
+            throw new UnsupportedOperationException("must be prefixed with L or R");
+        }
     }
 }
