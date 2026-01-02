532a80d2067c02b3d98e5ad63fc30887e44af9d3
Dominik Jetzen <djetzen@users.noreply.github.com>
2026-01-02 15:14:25 +0100

    implement secondpart of first day

--- src/main/java/de/advent/day1/Dial.java
+++ src/main/java/de/advent/day1/Dial.java
@@ -4,7 +4,7 @@ class Dial {
 
     private int currentValue = 50;
 
-    void rotate(String input) {
+    void rotateWithoutCountingZeroes(String input) {
         var rotationDirection = input.substring(0, 1);
         if (rotationDirection.equals("L")) {
             rotateLeft(Integer.parseInt(input.substring(1)));
@@ -13,6 +13,31 @@ class Dial {
         } else {
             throw new UnsupportedOperationException("must be prefixed with L or R");
         }
+    }
+
+    int rotateAndReturnNumberOfTimesPointedToZero(String input) {
+        var numberOfTimesZeroPassed = 0;
+        var rotationDirection = input.substring(0, 1);
+        if (rotationDirection.equals("L")) {
+            var valueToRotate = Integer.parseInt(input.substring(1));
+            for (int i = 0; i < valueToRotate; i++) {
+                rotateLeft(1);
+                if (getCurrentValue() == 0) {
+                    numberOfTimesZeroPassed++;
+                }
+            }
+        } else if (rotationDirection.equals("R")) {
+            var valueToRotate = Integer.parseInt(input.substring(1));
+            for (int i = 0; i < valueToRotate; i++) {
+                rotateRight(1);
+                if (getCurrentValue() == 0) {
+                    numberOfTimesZeroPassed++;
+                }
+            }
+        } else {
+            throw new UnsupportedOperationException("must be prefixed with L or R");
+        }
+        return numberOfTimesZeroPassed;
     }
 
     private void rotateLeft(int value) {
--- src/test/java/de/advent/day1/DialTest.java
+++ src/test/java/de/advent/day1/DialTest.java
@@ -1,59 +1,81 @@
 package de.advent.day1;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class DialTest {
 
-    @Test
-    void rotate_notStartingWithLOrR_throwsUnsupportedOperationException() {
-        var dial = new Dial();
+    public static Stream<Arguments> rotateAndCountingZeroes_args() {
+        return Stream.of(
+                arguments("R150", 2),
+                arguments("L150", 2)
+        );
+    }
 
-        assertThatThrownBy(() -> dial.rotate("55")).isInstanceOf(UnsupportedOperationException.class)
-                .hasMessage("must be prefixed with L or R");
+    public static Stream<Arguments> rotateWithoutCountingZeroes_args() {
+        return Stream.of(
+                arguments("L5", 45),
+                arguments("R5", 55),
+                arguments("R55", 5)
+        );
+
     }
 
     @Test
-    void rotateLeftSubtractsValue() {
+    void rotate_WithoutCountingZeroes_notStartingWithLOrR_throwsUnsupportedOperationException() {
         var dial = new Dial();
 
-        dial.rotate("L5");
+        assertThatThrownBy(() -> dial.rotateWithoutCountingZeroes("55")).isInstanceOf(UnsupportedOperationException.class)
+                .hasMessage("must be prefixed with L or R");
+    }
+
+    @ParameterizedTest
+    @MethodSource("rotateWithoutCountingZeroes_args")
+    void rotateWithoutCountingZeroes(String input, int expectedValue) {
+        var dial = new Dial();
+
+        dial.rotateWithoutCountingZeroes("L5");
 
         assertThat(dial.getCurrentValue()).isEqualTo(45);
     }
 
     @Test
-    void rotateRightAddsValue() {
+    void rotateAndReturnNumberOfTimesPointedToZero_notStartingWithLOrR_throwsUnsupportedOperationException() {
         var dial = new Dial();
 
-        dial.rotate("R5");
+        assertThatThrownBy(() -> dial.rotateAndReturnNumberOfTimesPointedToZero("55")).isInstanceOf(UnsupportedOperationException.class)
+                .hasMessage("must be prefixed with L or R");
+    }
 
-        assertThat(dial.getCurrentValue()).isEqualTo(55);
+    @ParameterizedTest
+    @MethodSource("rotateAndCountingZeroes_args")
+    void rotateAndReturnNumberOfTimesPointedToZero_returnsNumberOfZeroes(String input, int expectedValue) {
+        var dial = new Dial();
+
+        var numberOfTimesPointedToZero = dial.rotateAndReturnNumberOfTimesPointedToZero(input);
+
+        assertThat(numberOfTimesPointedToZero).isEqualTo(expectedValue);
     }
 
     @Test
-    void rotate_usesModulo100() {
-        var dial = new Dial();
-
-        dial.rotate("R55");
-
-        assertThat(dial.getCurrentValue()).isEqualTo(5);
-    }
-
-    @Test
-    void rotateTestInput_and_countZeroes() throws IOException {
+    void firstPart_solution() throws IOException {
         var dial = new Dial();
         var lines = Files.readAllLines(Paths.get("src/test/resources/day1/input-first-part.txt"));
         var zeroes = 0;
 
         for (var line : lines) {
-            dial.rotate(line);
+            dial.rotateWithoutCountingZeroes(line);
             if (dial.getCurrentValue() == 0) {
                 zeroes++;
             }
@@ -61,4 +83,19 @@ class DialTest {
 
         assertThat(zeroes).isEqualTo(1129);
     }
+
+    @Test
+    void secondPart_solution() throws IOException {
+        var dial = new Dial();
+        var lines = Files.readAllLines(Paths.get("src/test/resources/day1/input-first-part.txt"));
+        var zeroes = 0;
+
+        for (var line : lines) {
+            zeroes += dial.rotateAndReturnNumberOfTimesPointedToZero(line);
+        }
+
+        assertThat(zeroes).isEqualTo(6638);
+    }
+
+
 }
